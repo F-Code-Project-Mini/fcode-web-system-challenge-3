f57a342034bddf057d5049ccf3267ad5effad3bc
hoangtuanqn <phamhoangtuanqn@gmail.com>
2026-01-03 16:41:47 +0700

    Feat: Add mentor routes and API for barem scoring system

--- frontend/src/components/Header/Candidate.tsx
+++ frontend/src/components/Header/Candidate.tsx
@@ -1,20 +1,20 @@
 import { NavLink } from "./NavLink";
-import { Send, ServerCrash } from "lucide-react";
-import { useLocation } from "react-router";
-import Helper from "~/utils/helper";
+import { ServerCrash } from "lucide-react";
+// import { useLocation } from "react-router";
+// import Helper from "~/utils/helper";
 
 const CandidateHeader = () => {
-    const location = useLocation();
+    // const location = useLocation();
     return (
         <>
-            <li id="submissions">
+            {/* <li id="submissions">
                 <NavLink
                     url="/submissions"
                     name="Nộp đề tài"
                     Icon={Send}
                     active={Helper.isActive(location.pathname, "/submissions")}
                 />
-            </li>
+            </li> */}
 
             <li>
                 <NavLink url="https://discord.gg/WvudrJaYD" name="Hỗ trợ" Icon={ServerCrash} target="_blank" />
